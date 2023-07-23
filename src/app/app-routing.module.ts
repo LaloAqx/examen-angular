01574f9fa21f1f9ecbd09da9086c65d872e01059
JovannyRch <jovannyrch@gmail.com>
2023-07-22 21:07:40 -0600

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { BlogComponent } from './blog/blog.component';
+import { PokemonesComponent } from './pokemones/pokemones.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'blog', pathMatch: 'full' },
   { path: 'blog', component: BlogComponent },
+  { path: 'blogs', component: BlogComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'pokemones', component: PokemonesComponent },
+  { path: 'users', component: UsuariosComponent },
 ];
 
 @NgModule({
